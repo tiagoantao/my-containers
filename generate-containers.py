180cd277b22a06f7b80e9f58317e8f6efec5a6f1
Tiago Antao <tiagoantao@gmail.com>
2014-03-12 13:32:49 +0000

    lacking FROM

--- generate-containers.py
+++ generate-containers.py
@@ -6,8 +6,11 @@ containers = {
 
 def generate_container(container, machine, content):
     w = open(container, 'w')
+    w.write('FROM %s\n' % machine)
+    w.write('MAINTAINER Tiago Antao <tra@popgen.net>\n\n')
     for fname in content:
         w.write(open(fname).read())
+        w.write('\n')
     w.close()
 
 for name, content in containers.items():
