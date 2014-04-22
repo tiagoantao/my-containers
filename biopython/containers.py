c02b9b324a56f0c11b3dfeb8c6c43e27a62f6c87
Tiago Antao <tiagoantao@gmail.com>
2014-04-22 15:39:09 +0100

    Biopython on python3.4

--- biopython/containers.py
+++ biopython/containers.py
@@ -1,6 +1,11 @@
-containers = {
-    'Biopython': (['Biopython-Basic', 'Biopython-Run'],
-                  {'DIST': 'ubuntu:saucy', 'python': 'python'}),
-    'Biopython-Test': (['Biopython-Basic', 'Biopython-BuildBot'],
-                       {'DIST': 'ubuntu:saucy', 'python': 'python'}),
-}
+containers = {}
+for number, dist, lib_dir in [('', 'ubuntu:saucy', 'python2.7'),
+                              ('3', 'ubuntu:trusty', 'python3.4')]:
+    containers['Biopython%s' % number] = (
+        ['Biopython-Basic', 'Biopython-Run'],
+        {'DIST': dist, 'python': 'python%s' % number,
+         'python_dir': lib_dir})
+    containers['Biopython%s-Test' % number] = (
+        ['Biopython-Basic', 'Biopython-BuildBot'],
+        {'DIST': dist, 'python': 'python%s' % number,
+         'python_dir': lib_dir})
