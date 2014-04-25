127b5a97d58b9778f76784bd2939979e08b25711
Tiago Antao <tiagoantao@gmail.com>
2014-04-25 13:33:09 +0100

    python3

--- biopython/containers.py
+++ biopython/containers.py
@@ -1,11 +1,12 @@
 containers = {}
-for number, dist, lib_dir, pg in [('', 'saucy', 'python2.7', '9.1'),
-                              ('3', 'trusty', 'python3.4', '9.3')]:
+for number, dist, lib_dir, pg, pil in [
+    ('', 'saucy', 'python2.7', '9.1', ''),
+    ('3', 'trusty', 'python3.4', '9.3', 'python3-pil')]:
     containers['Biopython%s' % number] = (
         ['Biopython-Basic', 'Biopython-Run'],
         {'DIST': dist, 'python': 'python%s' % number, 'pg': pg,
-         'python_dir': lib_dir, 'pip': 'pip%s' % number})
+         'pil': pil, 'python_dir': lib_dir, 'pip': 'pip%s' % number})
     containers['Biopython%s-Test' % number] = (
         ['Biopython-Basic', 'Biopython-BuildBot'],
         {'DIST': dist, 'python': 'python%s' % number, 'pg': pg,
-         'python_dir': lib_dir, 'pip': 'pip%s' % number})
+         'pil': pil, 'python_dir': lib_dir, 'pip': 'pip%s' % number})
