bb0226e02f974c9673f0d786f1e68af06d34c470
Tiago Antao <tiagoantao@gmail.com>
2015-03-30 00:12:42 +0100

    vivid

--- biopython/containers.py
+++ biopython/containers.py
@@ -1,23 +1,27 @@
 containers = {}
-for number, dist, lib_dir, pg, pil in [
-    ('2', 'vivid', 'python2.7', '9.3', ''),
-    ('3', 'vivid', 'python3.4', '9.3', 'python3-pil')]:
+for default, number, dist, lib_dir, pg, pil in [
+    ('', '2', 'vivid', 'python2.7', '9.3', ''),
+    ('3', '3', 'vivid', 'python3.4', '9.3', 'python3-pil')]:
 
     containers['Biopython%s' % number] = (
         ['Biopython-Basic', 'Biopython-Run'],
         {'DIST': dist, 'python': 'python%s' % number, 'pg': pg,
+         'pythonp': 'python%s' % default,
          'pil': pil, 'python_dir': lib_dir, 'pip': 'pip%s' % number})
     containers['Biopython%s-Notebook' % number] = (
         ['Biopython-Basic', 'Biopython-Run', 'Biopython-nb'],
         {'DIST': dist, 'python': 'python%s' % number, 'pg': pg,
+         'pythonp': 'python%s' % default,
          'pil': pil, 'python_dir': lib_dir, 'pip': 'pip%s' % number,
          'ver': '%s' % number})
     containers['Biopython%s-Tutorial' % number] = (
         ['Biopython-Basic', 'Biopython-Run', 'Biopython-basic-tutorial'],
         {'DIST': dist, 'python': 'python%s' % number, 'pg': pg,
+         'pythonp': 'python%s' % default,
          'pil': pil, 'python_dir': lib_dir, 'pip': 'pip%s' % number,
          'ver': '%s' % number})
     containers['Biopython%s-Test' % number] = (
         ['Biopython-Basic', 'Biopython-BuildBot'],
         {'DIST': dist, 'python': 'python%s' % number, 'pg': pg,
+         'pythonp': 'python%s' % default,
          'pil': pil, 'python_dir': lib_dir, 'pip': 'pip%s' % number})
